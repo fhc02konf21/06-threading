9924e434beb35ceb238c94c8e586b7c1e35e4424
Stephan Kochauf <kc@htl-kaindorf.at>
2022-04-23 14:19:55 +0200

    isPrim

--- src/threading03/PrimValidation.java
+++ src/threading03/PrimValidation.java
@@ -1,6 +1,6 @@
 package threading03;
 
-public class PrimValidation {
+public class PrimValidation implements Runnable {
 
     private final long number;
 
@@ -9,9 +9,28 @@ public class PrimValidation {
         this.number = number;
     }
 
-    private void validate(){
-
-
+    @Override
+    public String toString() {
+        return "PrimValidation{" +
+                "number=" + number +
+                ", isPrim=" + isPrim +
+                '}';
     }
 
+    private void validate(){
+
+        isPrim = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrim = false;
+                break;
+            }
+        }
+        System.out.println("done:" + number + " is a Prim: " + isPrim);
+    }
+
+    @Override
+    public void run() {
+        validate();
+    }
 }
--- src/threading03/PrimValidiationDemoApp.java
+++ src/threading03/PrimValidiationDemoApp.java
@@ -13,6 +13,37 @@ public class PrimValidiationDemoApp {
         PrimValidation prim6 = new PrimValidation(1991999993);
         PrimValidation prim7 = new PrimValidation(1991999999);
 
+        Thread th1 = new Thread(prim1);
+        Thread th2 = new Thread(prim2);
+        Thread th3 = new Thread(prim3);
+        Thread th4 = new Thread(prim4);
+        Thread th5 = new Thread(prim5);
+        Thread th6 = new Thread(prim6);
+        Thread th7 = new Thread(prim7);
+
+        th1.start();
+        th2.start();
+        th3.start();
+        th4.start();
+        th5.start();
+        th6.start();
+        th7.start();
+
+        th1.join();
+        th2.join();
+        th3.join();
+        th4.join();
+        th5.join();
+        th6.join();
+        th7.join();
+
+        System.out.println("prim1 = " + prim1);
+        System.out.println("prim2 = " + prim2);
+        System.out.println("prim3 = " + prim3);
+        System.out.println("prim4 = " + prim4);
+        System.out.println("prim5 = " + prim5);
+        System.out.println("prim6 = " + prim6);
+        System.out.println("prim7 = " + prim7);
 
     }
 }
