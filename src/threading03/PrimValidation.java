8d0bcdc5c35c94731ae48f1f67a8350bb52dab44
Stephan Kochauf <kc@htl-kaindorf.at>
2022-04-23 14:00:33 +0200

    isPrim

--- src/threading03/PrimValidation.java
+++ src/threading03/PrimValidation.java
@@ -4,11 +4,13 @@ public class PrimValidation {
 
     private final long number;
 
+    private boolean isPrim;
     public PrimValidation(long number) {
         this.number = number;
     }
 
     private void validate(){
+
 
     }
 
